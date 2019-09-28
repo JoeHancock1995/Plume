badbfb7fa25b911971e93f9c0606777ab55d2b39
Joe Hancock <joe.hancock95@gmail.com>
2019-09-28 06:51:07 -0400

    added threejs

---
+++ assets/js/app.js
@@ -0,0 +1,135 @@
+
+const staggerVisualizerEl = document.querySelector('.stagger-visualizer');
+const fragment = document.createDocumentFragment();
+const grid = [18, 9];
+const col = grid[0];
+const row = grid[1];
+const numberOfElements = col * row;
+for (let i = 0; i < numberOfElements; i++) {
+  fragment.appendChild(document.createElement('div'));
+}
+staggerVisualizerEl.appendChild(fragment);
+const staggersAnimation = anime.timeline({
+  targets: '.stagger-visualizer div',
+  easing: 'easeInOutSine',
+  delay: anime.stagger(50),
+  loop: false,
+  autoplay: false
+})
+
+.add({
+  translateX: anime.stagger('1rem', {grid: grid, from: 'center', axis: 'x'}),
+  translateY: anime.stagger('1rem', {grid: grid, from: 'center', axis: 'y'}),
+  rotate: 0,
+  scaleX: 1,
+  scaleY: .25,
+  delay: anime.stagger(4, {from: 'center'})
+})
+.add({
+    boxShadow: [
+        '0px 0px 0px 0px rgba(0,0,0,.5)',
+        '20px 20px 10px 0px rgba(0,0,0,.5)'
+      ],
+  translateX: 0,
+  translateY: 0,
+  scale: .5,
+  scaleX: 1,
+  rotate: 0,
+  duration: 1000,
+  delay: anime.stagger(100, {grid: grid, from: 'center'})
+})
+.add({
+    boxShadow: [
+        '0px 0px 0px 0px rgba(0,0,0,.5)',
+        '20px 20px 10px 0px rgba(0,0,0,.5)'
+      ],
+  scaleY: 1,
+  scale: 1,
+  delay: anime.stagger(20, {grid: grid, from: 'center'})
+})
+.add({
+    boxShadow: [
+        '0px 0px 0px 0px rgba(0,0,0,.5)',
+        '20px 20px 10px 0px rgba(0,0,0,.5)'
+      ],
+    translateX: 0,
+    translateY: 0,
+    scale: .5,
+    scaleX: 1,
+    rotate: 0,
+    duration: 1000,
+    delay: anime.stagger(100, {grid: grid, from: 'center'})
+  })
+  .add({
+    scaleY: 1,
+    scale: 1,
+    delay: anime.stagger(20, {grid: grid, from: 'center'})
+  })
+  .add({
+    backgroundColor: '#000000',
+    delay: anime.stagger(20, {grid: grid, from: 'center'})
+  })
+  .add({
+    backgroundColor: '##00FF00',
+    delay: anime.stagger(20, {grid: grid, from: 'center'})
+  })
+.add({
+    Color:'#ffffff',
+    translateY: anime.stagger('10rem', {grid: grid, from: 'center', axis: 'y'}),
+    targets:'.hidden div',
+        scaleY: 1,
+        scale: 1,
+        })
+.add({
+    translateY:anime.stagger('9rem', {grid:grid, from:'center', axis: 'y' }),
+    targets:'.video-container div',
+    scaleY: 1
+});
+                            
+staggersAnimation.play();
+
+{
+    const MathUtils = {
+        lineEq: (y2, y1, x2, x1, currentVal) => {
+            // y = mx + b 
+            var m = (y2 - y1) / (x2 - x1), b = y1 - m * x1;
+            return m * currentVal + b;
+        },
+        lerp: (a, b, n) =>  (1 - n) * a + n * b
+    };
+
+};
+
+$(document).ready(function(){
+    $(".hidden").show(6000);
+    });
+
+
+    $(document).ready(function(){
+        $(".video1").show(8000);
+        });
+
+
+        var scene = new THREE.Scene();
+      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.body.appendChild( renderer.domElement );
+    
+      var geometry = new THREE.TetrahedronGeometry( 4, 6, 1 );
+      var material = new THREE.LineDashedMaterial( { color: 0xffffff } );
+      var cube = new THREE.Line( geometry, material );
+      scene.add( cube );
+    
+      camera.position.z = 12;
+      
+    
+      function animate() {
+        requestAnimationFrame( animate );
+        cube.rotation.x += 0.001;
+        cube.rotation.y += 0.001;
+        renderer.render ( scene, camera );
+      }
+      animate();
+
